fc65af5e626a7e104b5dbb1dec6f83c53ddaadb3
Denis Vasyukov <den365_1@mail.ru>
2025-02-02 06:24:54 +1030

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+function myMenuFunction() {
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if (menuBtn.className == 'nav-menu') {
+        menuBtn.className += ' responsive';
+
+    }
+    else {
+        menuBtn.className = 'nav-menu';
+    }
+}
+
+window.onscroll = function () { headerShadow() };
+let typeEffect = new Typed(".typedText", {
+    strings: [" менеджер вайлдберриз", " WB VIETNAM"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1200
+})
+var sr = ScrollReveal();
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured-name', { delay: 100 })
+sr.reveal('.featured-text-info', { delay: 200 })
+sr.reveal('.featured-text-btn', { delay: 200 })
+sr.reveal('.featured-image', { delay: 200 })
+sr.reveal('.social_icons', { delay: 200 })
+sr.reveal('.featured-image', { delay: 300 })
+sr.reveal('.project-box', { delay: 200 })
+sr.reveal('.top-header', {})
+
+const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+srLeft.reveal('.about-info', { delay: 100 })
+srLeft.reveal('.contact-info', { delay: 100 })
+
+const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+srRight.reveal('.skills-box', { delay: 100 })
+srRight.reveal('.form-control', { delay: 100 })
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+const scrollY = window.scrollY;
+
+sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute('id');
+  
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav-menu a[href*="' + '#' + sectionId + '"]').classList.add('active-link');
+    } else {
+      document.querySelector('.nav-menu a[href*="' + '#' + sectionId + '"]').classList.remove('active-link');
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+function headerShadow() {
+    const navHeader = document.getElementById('header');
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else {
+        navHeader.style.boxShadow = "none";
+        navHeader.style.boxShadow = "90px";
+        navHeader.style.boxShadow = "90px";
+    }
+
+}
